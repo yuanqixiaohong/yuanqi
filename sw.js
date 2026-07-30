74025d077c3608cbba5e30a415fe52890002fc91
yuanqixiaohong <1342477026@qq.com>
2026-07-30 19:36:11 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -80,7 +80,7 @@ self.addEventListener('fetch', (event) => {
         return fetchResponse;
       }).catch(() => {
         // Fallback to cached main page when offline or timeout
-        return caches.match('./workspace_optimized.html').then((cached) => {
+        return caches.match('./index.html').then((cached) => {
           if (cached) return cached;
           // Absolute last resort: if nothing cached, return a simple offline page
           return new Response(
